45b00ab310f0287dbbaff43174817a92d8d500df
EmmanuelBalde3434 <johan0balderas@gmail.com>
2025-10-03 20:52:37 -0600

    feat(pwa): agrega manifest.json, service worker y registro en main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
@@ -8,3 +9,12 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("Service Worker registrado"))
+      .catch(err => console.error("Error registrando SW:", err));
+  });
+}
